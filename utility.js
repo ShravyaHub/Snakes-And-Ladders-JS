c927e6cf6062b52d17796d61f1136d51187dc6f9
ShravyaHub <shravyak710@gmail.com>
2020-12-11 09:18:06 +0530

    ADDED: code to roll dice

--- utility.js
+++ utility.js
@@ -3,6 +3,11 @@ const START_POSITION = 0;
 module.exports = {
     startGame: function start() {
         var playerPosition = START_POSITION;
-        return playerPosition;
+        return rollDice();
     }
 };
+
+function rollDice() {
+    let diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+    return diceNumber;
+}
