6e7c63007ec01470964af3c54caf8753ecf0b820
ShravyaHub <shravyak710@gmail.com>
2020-12-11 09:37:51 +0530

    ADDED: code to check for options no play, ladder or snake

--- utility.js
+++ utility.js
@@ -1,13 +1,31 @@
 const START_POSITION = 0;
+const NO_PLAY = 1;
+const LADDER = 2;
+const SNAKE = 3;
 
 module.exports = {
     startGame: function start() {
         var playerPosition = START_POSITION;
-        return rollDice();
+        return rollDice(playerPosition);
     }
 };
 
-function rollDice() {
+function rollDice(playerPosition) {
     let diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
-    return diceNumber;
+    return options(playerPosition, diceNumber);
+}
+
+function options(playerPosition, diceNumber) {
+    let options = Math.floor(Math.random() * 10) % 3 + 1;
+    switch(options) {
+        case 1:
+            return "Player lost a turn, Player is at position " + playerPosition;
+        case 2:
+            playerPosition += diceNumber;
+            return "Player landed on a ladder, Player is at position " + playerPosition;
+        case 3:
+            playerPosition -= diceNumber;
+            return "Player landed on a snake, Player is at position " + playerPosition
+
+    }
 }
