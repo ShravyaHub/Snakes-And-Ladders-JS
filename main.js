b8ab7cf9b24eea1008ef0a7c2a148241c0db6deb
ShravyaHub <shravyak710@gmail.com>
2020-12-11 09:09:59 +0530

    ADDED: code to set player position at zero

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 var readlineSync = require('readline-sync');
+var utility = require('./utility');
 
 readlineSync.question("Welcome to Snakes and Ladders game!");
+console.log(utility.startGame());
+
---
+++ utility.js
@@ -0,0 +1,8 @@
+const START_POSITION = 0;
+
+module.exports = {
+    startGame: function start() {
+        var playerPosition = START_POSITION;
+        return playerPosition;
+    }
+};
